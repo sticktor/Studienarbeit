340a532da92a11c5b83235c99685d2a511f19ecf
sticktor <timroethel@web.de>
2020-04-07 00:19:42 +0200

    JVM Daten-Referenztypen

--- src/test/java/test/BadCase.java
+++ src/test/java/test/BadCase.java
@@ -1,9 +1,14 @@
 package test;
 
 class BadCase {
-    public Object method(Tester test) {
-        test.doSmth();
-
-        return new Object();
+    public void method() {
+        outer:
+        for (int i = 2; i < 1000; i++) {
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0)
+                    continue outer;
+            }
+            System.out.println (i);
+        }
     }
 }
